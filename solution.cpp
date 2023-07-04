26dd72261f718a398f8755bc139e23f7f4cdd5ce
souravdevloperr <138157343+souravdevloperr@users.noreply.github.com>
2023-07-05 01:36:21 +0600

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <cstdio>
+#include<iomanip>
+using namespace std;
 
+int main() {
+    int a;
+    long long b;
+    char ch;
+    float f;
+    double d;
+    scanf("%d %lld %c %f %lf",&a, &b, &ch, &f, &d);
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<ch<<endl;
+    cout<<setprecision(16)<<f<<endl;//they take 16 digit
+    
+    cout<<d<<endl;
+    return 0;
+}
